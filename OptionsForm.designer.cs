21ecade9bb5857ed2902484cab022163aead158a
Dezi <ddzlfe@gmail.com>
2020-06-02 15:40:44 -0400

    upload

---
+++ OptionsForm.designer.cs
@@ -0,0 +1,142 @@
+﻿namespace BlackJack
+{
+    partial class OptionsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.optionsLabel = new System.Windows.Forms.Label();
+            this.playerNameTextBox = new System.Windows.Forms.TextBox();
+            this.playerNameLabel = new System.Windows.Forms.Label();
+            this.selectFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.defaultImageList = new System.Windows.Forms.ImageList(this.components);
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // optionsLabel
+            // 
+            this.optionsLabel.AutoSize = true;
+            this.optionsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.optionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionsLabel.ForeColor = System.Drawing.Color.White;
+            this.optionsLabel.Location = new System.Drawing.Point(24, 9);
+            this.optionsLabel.Name = "optionsLabel";
+            this.optionsLabel.Size = new System.Drawing.Size(322, 39);
+            this.optionsLabel.TabIndex = 0;
+            this.optionsLabel.Text = "BlackJack Options";
+            // 
+            // playerNameTextBox
+            // 
+            this.playerNameTextBox.BackColor = System.Drawing.Color.Silver;
+            this.playerNameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::BlackJack.Properties.Settings.Default, "PlayerName", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.playerNameTextBox.Location = new System.Drawing.Point(226, 160);
+            this.playerNameTextBox.Name = "playerNameTextBox";
+            this.playerNameTextBox.Size = new System.Drawing.Size(171, 32);
+            this.playerNameTextBox.TabIndex = 1;
+            this.playerNameTextBox.Text = "Player 1";
+            // 
+            // playerNameLabel
+            // 
+            this.playerNameLabel.AutoSize = true;
+            this.playerNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.playerNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerNameLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.playerNameLabel.Location = new System.Drawing.Point(26, 162);
+            this.playerNameLabel.Name = "playerNameLabel";
+            this.playerNameLabel.Size = new System.Drawing.Size(146, 25);
+            this.playerNameLabel.TabIndex = 2;
+            this.playerNameLabel.Text = "Player Name";
+            this.playerNameLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // defaultImageList
+            // 
+            this.defaultImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.defaultImageList.ImageSize = new System.Drawing.Size(130, 130);
+            this.defaultImageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // okButton
+            // 
+            this.okButton.BackColor = System.Drawing.Color.Transparent;
+            this.okButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.okButton.FlatAppearance.BorderSize = 0;
+            this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.okButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.okButton.Location = new System.Drawing.Point(205, 433);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(108, 41);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = false;
+            this.okButton.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.BackColor = System.Drawing.Color.Transparent;
+            this.cancelButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cancelButton.FlatAppearance.BorderSize = 0;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(334, 433);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(108, 41);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            this.cancelButton.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // OptionsForm
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(625, 592);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.playerNameLabel);
+            this.Controls.Add(this.playerNameTextBox);
+            this.Controls.Add(this.optionsLabel);
+            this.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "OptionsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BlackJack Options";
+            this.Load += new System.EventHandler(this.OptionsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label optionsLabel;
+        private System.Windows.Forms.TextBox playerNameTextBox;
+        private System.Windows.Forms.Label playerNameLabel;
+        private System.Windows.Forms.OpenFileDialog selectFileDialog;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ImageList defaultImageList;
+    }
+}
+
